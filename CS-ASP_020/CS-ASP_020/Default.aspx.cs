3b2730d4aacc21b7b5b6a7b2ef513fd2558e7912
brawebdev <brawebdev@gmail.com>
2017-11-14 16:34:58 -0500

    Maintaining state with viewstate

--- CS-ASP_020/CS-ASP_020/Default.aspx.cs
+++ CS-ASP_020/CS-ASP_020/Default.aspx.cs
@@ -11,12 +11,20 @@ namespace CS_ASP_020
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!Page.IsPostBack)
+            {
+                ViewState.Add("MyValue", "");
+            }
         }
 
         protected void addButton_Click(object sender, EventArgs e)
         {
+            string value = ViewState["MyValue"].ToString();
+            value += valueTextBox.Text + " ";
+            ViewState["MyValue"] = value;
+            resultLabel.Text = value;
 
+            valueTextBox.Text = "";
         }
     }
 }
