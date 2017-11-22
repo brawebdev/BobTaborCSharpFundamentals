68b04cd6995ae4c3a9e9c615f73704b3cb8b927d
brawebdev <brawebdev@gmail.com>
2017-11-22 15:17:06 -0500

    Worked on adding to arrays

--- CS-ASP_023/CS-ASP_023/Default.aspx.cs
+++ CS-ASP_023/CS-ASP_023/Default.aspx.cs
@@ -11,12 +11,30 @@ namespace CS_ASP_023
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!Page.IsPostBack)
+            {
+                double[] hours = new double[0];
+                ViewState.Add("Hours", hours);
+            }
         }
 
         protected void addButton_Click(object sender, EventArgs e)
         {
+            double[] hours = (double[])ViewState["Hours"];
 
+            Array.Resize(ref hours, hours.Length + 1);
+
+            int newestItem = hours.GetUpperBound(0);
+
+            hours[newestItem] = double.Parse(hoursTextBox.Text);
+
+            ViewState["Hours"] = hours;
+
+            resultLabel.Text = String.Format("Total hours: {0}<br />Most Hours: {1}<br />Least Hours: {2}<br />Average Hours: {3:N2}", 
+                hours.Sum(),
+                hours.Max(),
+                hours.Min(),
+                hours.Average());
         }
     }
 }
